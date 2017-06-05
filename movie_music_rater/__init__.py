8df6a46893c544d651a1aa362ee98b30edb00aca
Jakub Jungiewicz <jungiewiczjakub@gmail.com>
2017-06-05 17:25:02 +0200

    Add files via upload

---
+++ movie_music_rater/__init__.py
@@ -0,0 +1,7 @@
+import movie_music_rater.MovieData
+import movie_music_rater.MusicData
+import movie_music_rater.Exceptions
+import movie_music_rater.tests
+import movie_music_rater.Main
+
+__all__ = ['MovieData', 'MusicData', 'Exceptions', 'tests', 'Main']
